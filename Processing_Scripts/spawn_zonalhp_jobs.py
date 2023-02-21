f1e1bf2de345cd9870e72353483416b18cb5bb72
Ryan Holmes <holmes.ryan.m@gmail.com>
2023-02-21 16:17:48 +1100

    Update plotting.

--- Processing_Scripts/spawn_zonalhp_jobs.py
+++ Processing_Scripts/spawn_zonalhp_jobs.py
@@ -7,11 +7,9 @@ import glob
 import sys
 
 # Files:
-experiments = ['05']
-# experiments = ['02','05','06','08','09','10','11','12','13',
-#               '15','16','17','18','19','20','21','24','25']
-# experiments = ['22','23']
-years = ['2015']#,'2016','2017','2018']
+experiments = ['02','05','06','08','09','10','11','12','13','14',
+              '15','16','17','18','19','20','21','24','25','26']
+years = ['2015','2016','2017','2018']
 
 for e in range(len(experiments)):
     for i in range(len(years)):
@@ -21,16 +19,7 @@ for e in range(len(experiments)):
 
         fscr = 'fscripts/Process_' + expstr + '_' + yrstr
 
-        # surface vars:
-        # os.system('cp zonalhp_sub.sub ' + fscr)
-        # with fileinput.FileInput(fscr, inplace=True) as file:
-        #     for line in file:
-        #         line_out = line.replace('XXEXPXX', expstr).replace('XXYRXX', yrstr)
-        #         print(line_out, end='')
-        # os.system('qsub ' + fscr)
-
-        # 3d vars:
-        os.system('cp zonalhp_3d_sub.sub ' + fscr)
+        os.system('cp zonalhp_sub.sub ' + fscr)
         with fileinput.FileInput(fscr, inplace=True) as file:
             for line in file:
                 line_out = line.replace('XXEXPXX', expstr).replace('XXYRXX', yrstr)
